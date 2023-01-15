f6c9931febf28d4f8ee65dd27a7cd89813162577
Sjors Jessen <sjors.jessen@movares.nl>
2023-01-15 16:53:43 +0100

    Added working Markdown to Pipe example

--- angular-workshop/src/app/app.component.ts
+++ angular-workshop/src/app/app.component.ts
@@ -18,10 +18,50 @@ export class AppComponent {
     {label: 'Cube', routerLink: ['/cube-three-js'] },
   ];
 
-  public content: string = 
-  `
-    ## @Input
+  public content: string = `# Angular workshop
+  Deze workshop is toegespitst op het visualiseren van 3D en data in web apps. Dit vanwege onze eigen werkzaamheden, maar ook met het oog op onze toekomstige samenwerking met het data team.
 
-    ![Input](https://angular.io/generated/images/guide/inputs-outputs/input.svg)
-  `;
+  ---
+
+  **Inhoudstabel**
+
+  - [Angular workshop](#angular-workshop)
+    - [Angular](#angular)
+    - [NPM en node packages](#npm-en-node-packages)
+    - [Angular installation](#angular-installation)
+    - [@Component](#component)
+    - [Modules](#modules)
+    - [Pipes](#pipes)
+    - [@Input](#input)
+    - [@Output](#output)
+    - [Services](#services)
+    - [Directives](#directives)
+      - [Directive Types](#directive-types)
+    - [Angular fundamentale concepten](#angular-fundamentale-concepten)
+    - [Observables](#observables)
+    - [Mapbox \& ngx-mapbox](#mapbox--ngx-mapbox)
+    - [ThreeJS \& NG3](#threejs--ng3)
+    - [PrimeNg (component library)](#primeng-component-library)
+    - [Nawoord](#nawoord)
+      - [Pluspunten](#pluspunten)
+      - [Minpunten](#minpunten)
+    - [Vervolg](#vervolg)
+
+  ## Angular
+
+  [Angular](https://angular.io/docs) is een application-design framework en development platform voor het ontwikkelen van efficiënte en complexe single-page applicaties.
+
+  **Traditionele website**
+  ![Traditioneel](https://br-cms.bloomreach.com/site/binaries/content/gallery/br_traditional_1.gif)
+
+  **Single-page applicatie (SPA)**
+  ![SPA](https://br-cms.bloomreach.com/site/binaries/content/gallery/br_singlepage_application_1.gif)
+
+  ## NPM en node packages
+
+  - Node package manager (NPM) is een manager waarmee je de node packages in je applicatie kunt beheren. Vergelijkbaar met Unity's package manager of Nuget package manager
+  - Installeer [Node.js](https://nodejs.org/en/) om node packages te kunnen installeren via NPM commands in de terminal
+  - Packages en diens versies worden bijgehouden in *package.json* (vergelijkbaar met Unity's manifest.json), hier kun je ook wijzigingen aan toe brengen
+  - Je installeert een nieuwe node package via terminal m.b.v. <code>npm install [package-naam]</code>, deze package wordt dan geïnstalleerd in de *node_modules* folder
+  `
 }
